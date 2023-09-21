2034e0eafb642c809c641e649b85d064d7fb067d
sit98 <chirag.v@shaligraminfotech.com>
2023-09-21 15:27:52 +0530

    add daata

--- app/src/main/java/com/example/cicd/MainActivity.kt
+++ app/src/main/java/com/example/cicd/MainActivity.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.cicd.ui.theme.CICDTheme
 
 class MainActivity : ComponentActivity() {
-    var str_Data : String = "dsdsdssds"
+    var str_Data : String = "dsdsdssds fdfdf"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
